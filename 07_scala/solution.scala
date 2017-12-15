ebe3377e8f59c26014764567c326022557258ac6
Vojtěch Jelínek <vojtin.j@gmail.com>
2017-12-15 20:15:47 +0100

    Seventh day part two

--- 07_scala/solution.scala
+++ 07_scala/solution.scala
@@ -1,25 +1,42 @@
 import scala.io.Source;
 
 object Solution {
-  def traverse_program(id: String, values: Map[String, Int], connections: Map[String, Set[String]]): Tuple2 = {
+  def traverse_program(id: String, values: Map[String, Int], connections: Map[String, Set[String]]): Tuple2[Int, Int] = {
 	if (connections contains id) {
-    var child_weight: Int = 0
-    var weight_sum: Int = 0
-    for (child <- connections(id)) {
-      val weight = traverse_program(child, values, connections)
-      if (child_weight > 0) {
-        if (weight != child_weight) {
-          val difference = child_weight - weight
+    var possible_child_weights: Tuple2[Int, Int] = (-1, -1)
+    var possible_child_weights_sums: Tuple2[Int, Int] = (-1, -1)
+    var childs_sums = 0
+    for ((child, i) <- connections(id).zipWithIndex) {
+      val weight_info: Tuple2[Int, Int] = traverse_program(child, values, connections)
+      val weight_sum: Int = weight_info._1 + weight_info._2
+      childs_sums += weight_sum
+      if (possible_child_weights_sums._1 == -1) {
+        possible_child_weights = possible_child_weights.copy(_1 = weight_info._2)
+        possible_child_weights_sums = possible_child_weights_sums.copy(_1 = weight_sum)
+      } else if (possible_child_weights_sums._2 == -1) {
+        possible_child_weights = possible_child_weights.copy(_2 = weight_info._2)
+        possible_child_weights_sums = possible_child_weights_sums.copy(_2 = weight_sum)
+      } else if (possible_child_weights_sums._2 == possible_child_weights_sums._1) {
+        if (weight_sum != possible_child_weights_sums._1) {
+          val difference = weight_sum - possible_child_weights._1
+          println(weight_sum + difference)
           System.exit(0)
         }
       } else {
-        child_weight = weight
+        if (weight_sum == possible_child_weights_sums._1) {
+          val difference = possible_child_weights_sums._2 - weight_sum
+          println(possible_child_weights._2 - difference)
+          System.exit(0)
+        } else if (weight_sum == possible_child_weights_sums._2) {
+          val difference = possible_child_weights_sums._1 - weight_sum
+          println(possible_child_weights._1 - difference)
+          System.exit(0)
+        }
       }
-      weight_sum += weight
     }
-    weight_sum + values(id)
+    (childs_sums, values(id))
   } else {
-    values(id)
+    (0, values(id))
   }
 }
 
